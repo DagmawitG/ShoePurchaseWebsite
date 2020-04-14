ecc4e60953cfb8992022b4d5131d5eecec5268de
Dagmawit Getachew <se.dagmawit.getachew@gmail.com>
2020-04-14 16:36:02 +0300

    first version 1.0.0

---
+++ assets/css/css/practiceJs.js
@@ -0,0 +1,5 @@
+var name = prompt("What is your name?");
+alert("Your name is "+ name );
+var name,age,isMarried;
+name="Kebede",age=12,isMarried=true;
+console.log(name+'is'+age+'years old'+'is he married'+isMarried);
