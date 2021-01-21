cc60456f9385b8cef965dde840e293f66e261d98
Dagi Get <dagiget1@gmail.com>
2021-01-22 02:16:00 +0300

    Third version 3.0.0 (creation of about page)

--- assets/css/css/practiceJs.js
+++
@@ -1,5 +0,0 @@
-var name = prompt("What is your name?");
-alert("Your name is "+ name );
-var name,age,isMarried;
-name="Kebede",age=12,isMarried=true;
-console.log(name+'is'+age+'years old'+'is he married'+isMarried);
